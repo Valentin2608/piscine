cb668febe4478118ca66e7edf9b68ac1fb873cd5
Valentin2608 <valentin.guisnet@edu.ece.fr>
2020-04-13 15:57:45 +0200

    ajout connexion

---
+++ connexion.php
@@ -0,0 +1,40 @@
+<?php
+
+ $login = isset($_POST["login"])? $_POST["login"] : ""; //if then else
+ $password = isset($_POST["password"])? $_POST["password"] : "";
+
+
+//identifier votre BDD
+$database = "EbayECE";
+//connectez-vous de votre BDD
+$db_handle = mysqli_connect('localhost', 'root', '');
+$db_found = mysqli_select_db($db_handle, $database);
+
+//Partie 1:recherche login et mot de passe dans la BDD
+//*****************************
+
+if (isset($_POST['button1'])) {
+	if ($db_found) {
+		$sql = "SELECT * FROM acheteur";
+		if ($login != "") {
+			$sql .= " WHERE Email LIKE '%$login%'";
+			if ($password != "") {
+				$sql .= " AND Password LIKE '$password'";
+								}
+							}
+		$result = mysqli_query($db_handle, $sql);
+		//regarder s'il y a de résultat
+		if (mysqli_num_rows($result) == 0) {
+			echo "Compte non trouvé";
+				}
+				else {
+						
+					echo "Connexion réussie";
+
+						}
+}
+	else {
+		echo "Compte non trouvé";
+			}
+}
+?> 
