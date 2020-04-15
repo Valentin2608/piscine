d9fb9ceaf1eae403ecf034d7b75398078ad029c8
Valentin2608 <valentin.guisnet@edu.ece.fr>
2020-04-15 23:49:39 +0200

    dl image c'est fou

--- vendreitem.php
+++ vendreitem.php
@@ -1,39 +1,61 @@
+
 <?php
  $nom = isset($_POST["nom"])? $_POST["nom"] : ""; 
- 
  $description = isset($_POST["description"])? $_POST["description"] : "";
- $image = isset($_POST["image"])? $_POST["image"] : "";
+/*<form method="post" enctype="multipart/form-data" action="vendreitem.php" >
+            <input type="file" name="photo">
+            <input type="submit" name ="button1" value="Mettre Photo">
+       </form> */
   $categorie = isset($_POST["categorie"])? $_POST["categorie"] : "";
   $typeVente = isset($_POST["typeVente"])? $_POST["typeVente"] : ""; 
  $IDVendeur = "0";
   $prix = isset($_POST["prix"])? $_POST["prix"] : "";
+ 
+  
+if (isset($_POST['button1'])) {
+	
 
 
- $erreur = "";
+ $message = "";
  if ($nom== "") 
  {
- $erreur .= "Nom est vide. <br>"; }
+ $message = 'Nom est vide.';
+echo '<script type="text/javascript">window.alert("'.$message.'");</script>'; }
  if ($description == "") {
- $erreur .= "La Description est vide. <br>"; }
- if ($image == "") {
- $erreur .= "L'image est vide. <br>"; }
+ $message= "La Description est vide. <br>"; 
+ echo '<script type="text/javascript">window.alert("'.$message.'");</script>';}
  if ($categorie == "") {
- $erreur .= "La catégorie est vide. <br>"; }
+ $message= "La catégorie est vide. <br>"; 
+ echo '<script type="text/javascript">window.alert("'.$message.'");</script>';}
  if ($typeVente == "") {
- $erreur .= "Le Type de Vente est vide. <br>"; }
+ $message= "Le Type de Vente est vide. <br>"; 
+ echo '<script type="text/javascript">window.alert("'.$message.'");</script>';}
  if ($prix == "") {
- $erreur .= "Le prix est vide. <br>"; }
+ $message= "Le prix est vide. <br>"; 
+ echo '<script type="text/javascript">window.alert("'.$message.'");</script>';}
 
 
- if ($erreur == "") 
+ if ($message == "") 
  {
- 	$database = "EbayECE";
+ echo '<p>La photo2 a bien été envoyée.</p>';
+ 	$database = "ECEEbay";
+	
 	$db_handle = mysqli_connect('localhost', 'root', '');
 	$db_found = mysqli_select_db($db_handle, $database);
-	if ($_POST["button1"]) 
-	{
+		 
+		 // récupération et dl de la photo
+		 if (isset($_FILES['photo']['tmp_name'])) {
+		$_FILES['photo']['name']="alain.jpeg";
+		$retour = copy($_FILES['photo']['tmp_name'],"alain.jpeg");
 		
-		$sql="INSERT INTO `items`(`Nom`, `Description`, `Images`, `Categorie`, `TypedeVente`, `IDVendeur`, `Prix`) VALUES ('$nom','$description','$image','$categorie','$typeVente','$IDVendeur','$prix ')";
+        if($retour) {
+            echo '<p>La photo a bien été envoyée.</p>';
+            
+			echo '<img src="alain.jpeg">';
+        }
+    }	
+	
+		$sql="INSERT INTO `items`(`Nom`, `Description`, `Images`, `Categorie`, `TypedeVente`, `IDVendeur`, `Prix`) VALUES ('$nom','$description','caca','$categorie','$typeVente','$IDVendeur','$prix ')";
 		
 		if(mysqli_query($db_handle, $sql)){ 
     	echo "Record was updated successfully."; 
@@ -43,9 +65,11 @@
                             . mysqli_error($db_handle); 
 		}  
 	mysqli_close($db_handle); 
-	}
- }
- else {
- echo "Erreur : $erreur";
- }
+}
+ 
+ 
+ 
+ 
+}
+
 ?> 
